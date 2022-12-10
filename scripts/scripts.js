d97b62f1d0215b5992a5a1899cca50630bcf4388
PCB <perbjester@gmail.com>
2022-12-10 23:56:14 +0100

    xxx

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,35 @@
 // @ts-nocheck
 const buttons = document.querySelectorAll("[data-carousel-button]");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      ?.querySelector("[data-slides]");
+function loopSlidesForever() {
+  const slides = document.querySelector("[data-slides]");
+  const offset = 1;
+  setInterval(function () {
     const activeSlide = slides.querySelector("[data-active");
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
-  });
-});
+  }, 5000);
+}
+
+loopSlidesForever();
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+//     const slides = button
+//       .closest("[data-carousel]")
+//       ?.querySelector("[data-slides]");
+//     const activeSlide = slides.querySelector("[data-active");
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+//     if (newIndex < 0) newIndex = slides.children.length - 1;
+//     if (newIndex >= slides.children.length) newIndex = 0;
+//     slides.children[newIndex].dataset.active = true;
+//     delete activeSlide.dataset.active;
+//   });
+// });
 
 const login = () => {
   event.preventDefault();
